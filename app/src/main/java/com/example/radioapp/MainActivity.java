b7a24cf56f321d663e43a61062d60f5d309f15d7
unknown <u1065204@hk.edu.tw>
2020-03-30 10:43:28 +0800

    20200330 My favorite K Girl Group

--- app/src/main/java/com/example/radioapp/MainActivity.java
+++ app/src/main/java/com/example/radioapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.radioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnOK(View view) {
+        TextView txvLike = findViewById(R.id.txvShow);
+        RadioGroup kGrilGroup = findViewById(R.id.kGrilGroup);
+
+        int id = kGrilGroup.getCheckedRadioButtonId();
+        RadioButton selected = findViewById(id);
+
+        txvLike.setText("My favorite K Girl Group ：" + selected.getText());
     }
 }
